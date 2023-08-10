9d35e7b27b0cf197fbc3ac668f75a319a2b6f3bb
Kanji Hara Neto <hkanjih@gmail.com>
2023-08-10 09:54:03 -0300

    [LEARN001] First version

---
+++ core-customize/hybris/bin/custom/demobackoffice/backoffice/src/de/hybris/demo/services/DemobackofficeService.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved
+ */
+package de.hybris.demo.services;
+
+/**
+ * Hello World DemobackofficeService
+ */
+public class DemobackofficeService
+{
+	public String getHello()
+	{
+		return "Hello";
+	}
+}
---
+++ core-customize/hybris/bin/custom/demobackoffice/src/de/hybris/demo/constants/DemobackofficeConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved
+ */
+package de.hybris.demo.constants;
+
+@SuppressWarnings({"deprecation","squid:CallToDeprecatedMethod"})
+public final class DemobackofficeConstants extends GeneratedDemobackofficeConstants
+{
+	public static final String EXTENSIONNAME = "demobackoffice";
+	
+	private DemobackofficeConstants()
+	{
+		//empty
+	}
+	
+	
+}
